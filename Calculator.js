ea515328c295b61f58ea106f888b984cf132e7d1
Cryodine <44298573+Cryodine@users.noreply.github.com>
2018-12-04 11:03:00 -0800

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,22 @@
-function calc() {
+function init() {
+  
+  var zero = document.getElementById("zero");
+  var one = document.getElementById("one");
+  var two = document.getElementById("two");
+  var three = document.getElementById("three");
+  var four = document.getElementById("four");
+  var five = document.getElementById("five");
+  var six = document.getElementById("six");
+  var seven = document.getElementById("seven");
+  var eight = document.getElementById("eight");
+  var nine = document.getElementById("nine");
+  
+  var plus = document.getElementById("plus");
+  var equals = document.getElementById("equals");
+  
+  var product;
+}
 
-
-
-
+function add(var x,var y) {
+  return(x+y);
 }
